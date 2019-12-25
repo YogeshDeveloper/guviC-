e101a96b3190a82b05cc7f6c7c54e11e31f8bfea
YogeshDeveloper <spyogesh1994@gmail.com>
2019-12-25 23:52:05 +0530

    7.ninethTable.cpp

--- 7.ninethTable.cpp
+++ 7.ninethTable.cpp
@@ -4,9 +4,15 @@ int main()
 {
     int a = 0;
     cin>>a;
+    if (a=0)
+    {
+        cout<<"NULL";
+    }
+    else{
     for(int i=1;i<=3;i++)
     {
         cout<<a*i<< " ";
     }
+     }
     return 0;
 }
