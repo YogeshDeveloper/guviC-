c319799b10f36637e430d2e345d6b9e883c895c8
YogeshDeveloper <spyogesh1994@gmail.com>
2019-12-27 15:06:21 +0530

    11.smallerOfTwo.cpp

---
+++ 10.areaOfEquilateralTriangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    float a=0,area=0;
+    cin>>a;
+    cout<<a<<"\n";
+    area = 1/4*(sqrt(3*a*a));
+    cout<<area;
+    return 0;
+}
